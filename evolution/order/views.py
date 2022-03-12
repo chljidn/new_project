57bad494d017d9ee9c0fc71e61e2aa57a0299494
chljidn <chljidn@nate.com>
2022-03-13 00:54:43 +0900

    주문 앱

--- evolution/order/views.py
+++ evolution/order/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from order.models import basket, order
 from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 
 # Create your views here.
 # 장바구니
 class basket(viewsets.ViewSet):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
     queryset = basket.objects.all()
-    def list(self):
+    def filter_queryset(self, request, queryset):
+        return queryset.filter(username=request.user)
+    #
+    def retrieve(self, request, pk=None):
+        if request.user.is_authenticated:
+            basket_list = self.filter_queryset(request, self.queryset)
+            # serializer =
 
-        pass
     def update(self, request):
         pass
     def destroy(self, request):
