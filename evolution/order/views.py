94c6b72ea7e42d2d30ee338386dd64b38ea5887a
chljidn <chljidn@nate.com>
2022-03-13 15:34:34 +0900

    장바구니 기능(추가, 수정), 주문, 주문상세

--- evolution/order/views.py
+++ evolution/order/views.py
@@ -6,6 +6,7 @@ from rest_framework.authentication import SessionAuthentication, BasicAuthentica
 from order.serializers import basket_serializer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import action
 
 # Create your views here.
 # 장바구니
@@ -14,8 +15,9 @@ class basket(viewsets.ViewSet):
     queryset = basket_model.objects.all()
     def filter_queryset(self, request, queryset):
         return queryset.filter(username=request.user)
-    #
-    def retrieve(self, request, pk=None):
+
+    @action(detail=False, methods=['get'])
+    def my_basket(self, request):
         if request.user.is_authenticated:
             basket_list = self.filter_queryset(request, self.queryset)
             serializer = basket_serializer(basket_list, many=True)
@@ -26,7 +28,7 @@ class basket(viewsets.ViewSet):
             my_basket = basket_model.objects.filter(username=request.user, product_id = request.data['product'])
             product_id = product.objects.get(product_id=request.data['product'])
             if my_basket:
-                my_basket[0].count += 1
+                my_basket[0].count = request.data['count']
                 my_basket[0].save()
             else:
                 my_basket = basket_model.objects.create(
@@ -40,8 +42,19 @@ class basket(viewsets.ViewSet):
         else:
             return Response({"message": "로그인이 필요한 기능입니다."}, status=status.HTTP_401_UNAUTHORIZED)
 
-    def update(self, request):
-        pass
+    # 유저 아이디가 들어오면 그 아이디와 상품이름이 맞는걸 찾고, 엽데이트
+    def update(self, request, pk=None):
+        if request.user.is_authenticated:
+            my_basket = basket_model.objects.filter(username=request.user, product_id=request.data['product'])
+            my_basket.update(
+                username=request.user,
+                product_id=request.data['product'],
+                count = request.data['count']
+            )
+            serializer = basket_serializer(my_basket, many=True)
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
     def destroy(self, request):
+
+
         pass
