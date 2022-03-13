4a96a4440dfff89d7c4422265a23820f6f1b36fb
chljidn <chljidn@nate.com>
2022-03-13 12:35:03 +0900

    .

--- evolution/restaurant/serializers.py
+++ evolution/restaurant/serializers.py
@@ -5,10 +5,10 @@ from restaurant.models import restaurant, product
 class restaurant_serializer(serializers.ModelSerializer):
     class Meta:
         model = restaurant
-        fields = ()
+        fields = ('restaurant_id')
 
 class product_serializer(serializers.ModelSerializer):
-    restaurant_id = serializers.StringRelatedField(restaurant_serializer)
+    restaurant_id = serializers.StringRelatedField()
     class Meta:
         model = product
         fields = ('product_id', 'restaurant_id', 'product_name', 'price', 'category', 'image', 'content')
