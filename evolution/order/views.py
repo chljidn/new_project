6a08e2cd4902819745e4cf42b79535f4aa7ec8d2
chljidn <chljidn@nate.com>
2022-03-14 21:49:08 +0900

    장바구니 리스트, 추가, 변경

--- evolution/order/views.py
+++ evolution/order/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from order.models import basket as basket_model, order
 from restaurant.models import product
 from rest_framework import viewsets
@@ -23,6 +22,8 @@ class basket(viewsets.ViewSet):
             serializer = basket_serializer(basket_list, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
 
+    # 여러 객체가 들어올 경우로 수정 요망
+    # ex) [{상품번호:1, 이름: 네내치킨 후라이드, 수량:1}, {상품번호 : 5, 이름: 네네치킨 간장, 수량:2}]
     def create(self, request):
         if request.user.is_authenticated:
             my_basket = basket_model.objects.filter(username=request.user, product_id = request.data['product'])
@@ -42,10 +43,9 @@ class basket(viewsets.ViewSet):
         else:
             return Response({"message": "로그인이 필요한 기능입니다."}, status=status.HTTP_401_UNAUTHORIZED)
 
-    # 유저 아이디가 들어오면 그 아이디와 상품이름이 맞는걸 찾고, 엽데이트
     def update(self, request, pk=None):
         if request.user.is_authenticated:
-            my_basket = basket_model.objects.filter(username=request.user, product_id=request.data['product'])
+            my_basket = self.queryset.filter(username=request.user, product_id=request.data['product'])
             my_basket.update(
                 username=request.user,
                 product_id=request.data['product'],
@@ -55,6 +55,16 @@ class basket(viewsets.ViewSet):
             return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
     def destroy(self, request):
+        pass
 
+# 내 주문 목록(해당 유저의 모든 주문 목록 리스트)
+# 해당 주문의 상세 내역(해당 유저의 해당 주문의 상세 내역) < = 이 부분은 주문 상세로 가야 할 듯?
+# 결제 기능 추가해야 하므로 뒤로 미룸
 
+class order_view(viewsets.ViewSet):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    def create(self, request):
+        pass
+
+    def delete(self, request):
         pass
