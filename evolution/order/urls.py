8db450a40e39d040465940b739130887724f8508
chljidn <chljidn@nate.com>
2022-03-14 21:50:19 +0900

    url 수정

--- evolution/order/urls.py
+++ evolution/order/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 from order import views as order_views
 
 app_name = "order"
+
 router= DefaultRouter()
 router.register('basket', order_views.basket, basename='basket')
 
