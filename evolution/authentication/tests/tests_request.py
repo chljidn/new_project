71bf60f4bcb074d88a060e917840b92dbb70a676
chljidn <chljidn@nate.com>
2022-03-14 21:46:32 +0900

    인증 테스트

---
+++ evolution/authentication/tests/tests_request.py
@@ -0,0 +1,58 @@
+from rest_framework.test import APITestCase, APIClient
+from authentication.models import User
+from django.urls import reverse
+import re
+
+
+class test_auth_request(APITestCase):
+
+    session_id = None
+
+    def setUp(self):
+        user = User.objects.create_user(
+            username='setup',
+            password='setup1234',
+            sex='M',
+            birth='1991-01-10',
+            email='setup@setup.com')
+
+    def test_signup(self):
+        url = reverse('authentication:signup-list')
+        response = self.client.post(url, {
+            'username':'test',
+            'password':'test1234',
+            'sex':'M',
+            'birth':'1991-01-01',
+            'email':'test@test.com'
+        }, format='json')
+        self.assertEqual(response.status_code, 201)
+
+    def test_login(self):
+        client = APIClient()
+        login_url = reverse('authentication:signup-login')
+        response = client.post(login_url,{
+            "username":"setup",
+            "password":"setup1234"
+        }, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        # cls.session_id = re.findall("=(.+?);", str(response.cookies['sessionid']))[0]
+
+    def test_logout(self):
+        login_url = reverse('authentication:signup-login')
+        response = self.client.post(login_url,{
+            "username":"setup",
+            "password":"setup1234"
+        }, format='json')
+        session_id = re.findall("=(.+?);", str(response.cookies['sessionid']))[0]
+        logout_url = reverse('authentication:signup-logout')
+        response = self.client.post(logout_url,
+                                    headers={'sessionid': session_id})
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_update(self):
+        pass
+
+    def test_withdarawl(self):
+        pass
