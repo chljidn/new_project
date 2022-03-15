b3bf5314c503ef4845e7eb42f3b53c21629b7f39
chljidn <chljidn@nate.com>
2022-03-15 22:59:24 +0900

    주문 직렬화 추가

--- evolution/order/serializers.py
+++ evolution/order/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from authentication.serializers import user_serializer
 from restaurant.serializers import product_serializer
-from order.models import basket
+from order.models import basket, order, order_detail
 
 
 class basket_serializer(serializers.ModelSerializer):
@@ -10,3 +10,8 @@ class basket_serializer(serializers.ModelSerializer):
     class Meta:
         model = basket
         fields = ('basket_id', 'username', 'product_id', 'count')
+
+class order_serializer(serializers.ModelSerializer):
+    class Meta:
+        model = order
+        fields = ('order_id', 'order_time', 'prediction_time')
