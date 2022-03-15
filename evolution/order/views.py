b518ce9518cc83079e04085492c39da5767e59b1
chljidn <chljidn@nate.com>
2022-03-15 14:30:01 +0900

    장바구니 객체 리스트에 의한 업데이트 추가

--- evolution/order/views.py
+++ evolution/order/views.py
@@ -12,8 +12,8 @@ from rest_framework.decorators import action
 class basket(viewsets.ViewSet):
     authentication_classes = [SessionAuthentication, BasicAuthentication]
     queryset = basket_model.objects.all()
-    def filter_queryset(self, request, queryset):
-        return queryset.filter(username=request.user)
+    def filter_queryset(self, request):
+        return self.queryset.filter(username=request.user)
 
     @action(detail=False, methods=['get'])
     def my_basket(self, request):
@@ -23,21 +23,22 @@ class basket(viewsets.ViewSet):
             return Response(serializer.data, status=status.HTTP_200_OK)
 
     # 여러 객체가 들어올 경우로 수정 요망
-    # ex) [{상품번호:1, 이름: 네내치킨 후라이드, 수량:1}, {상품번호 : 5, 이름: 네네치킨 간장, 수량:2}]
+    # ex) {basket : {product_id:1, count:1}, {상품번호 : 3, 수량:2}}
     def create(self, request):
         if request.user.is_authenticated:
-            my_basket = basket_model.objects.filter(username=request.user, product_id = request.data['product'])
-            product_id = product.objects.get(product_id=request.data['product'])
-            if my_basket:
-                my_basket[0].count = request.data['count']
-                my_basket[0].save()
-            else:
-                my_basket = basket_model.objects.create(
-                    username=request.user,
-                    product_id=product_id,
-                    count=request.data['count']
-                )
-            serializer = basket_serializer(my_basket)
+            my_basket = basket_model.objects.filter(username_id=request.user) # 해당 유저의 장바구니 목록
+            for detail in request.data['basket_list']:
+                my_basket_detail = my_basket.filter(product_id=detail['product'])
+                if my_basket_detail:
+                    my_basket_detail[0].count = detail['count']
+                    my_basket_detail[0].save()
+                else:
+                    my_basket_detail = basket_model.objects.create( # 수정 요망
+                        username=request.user,
+                        product_id=detail['product'],
+                        count=detail['count']
+                    )
+                serializer = basket_serializer(my_basket, many=True)
             return Response(serializer.data)
 
         else:
@@ -60,7 +61,6 @@ class basket(viewsets.ViewSet):
 # 내 주문 목록(해당 유저의 모든 주문 목록 리스트)
 # 해당 주문의 상세 내역(해당 유저의 해당 주문의 상세 내역) < = 이 부분은 주문 상세로 가야 할 듯?
 # 결제 기능 추가해야 하므로 뒤로 미룸
-
 class order_view(viewsets.ViewSet):
     authentication_classes = [SessionAuthentication, BasicAuthentication]
     def create(self, request):
