d7a95bf2c0dbb090fc7b5215bef8c3e1aa447e28
chljidn <chljidn@nate.com>
2022-03-15 23:01:58 +0900

    주키 자동 생성 변경

--- evolution/order/models.py
+++ evolution/order/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from authentication.models import User
 from restaurant.models import product
-# Create your models here.
+from django.utils import timezone
 
 class basket(models.Model):
     basket_id = models.AutoField(primary_key=True)
@@ -10,13 +10,13 @@ class basket(models.Model):
     count = models.IntegerField()
 
 class order(models.Model):
-    order_id = models.IntegerField(primary_key=True)
+    order_id = models.AutoField(primary_key=True)
     username = models.ForeignKey(User, on_delete=models.CASCADE)
     order_time = models.DateTimeField()
     prediction_time = models.DateTimeField()
 
 class order_detail(models.Model):
-    order_detail_id = models.IntegerField(primary_key=True)
+    order_detail_id = models.AutoField(primary_key=True)
     order_id = models.ForeignKey(order, on_delete=models.CASCADE)
     product_id = models.ForeignKey(product, on_delete=models.PROTECT)
     count = models.IntegerField()
