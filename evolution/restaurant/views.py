84bddb8beb8d67a0a60d0c0c754c8988d99cd45d
chljidn <chljidn@nate.com>
2022-03-25 21:26:33 +0900

    가맹점주 view를 유저 view의 상속으로 처리

--- evolution/restaurant/views.py
+++ evolution/restaurant/views.py
@@ -10,59 +10,25 @@ from django.contrib.auth import login, logout, authenticate
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from django_filters.rest_framework import backends
 from restaurant.filters import restaurant_filter
+from authentication.views import user_auth
+from authentication.serializers import user_serializer
 
-class owner_auth_view(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = owner_serializer
-
-    # 전체 조회는 스태프만 가능.
-    def list(self, request):
-        if not request.user.IsAdminUser:
-            return Response({'해당 정보를 조회할 권한이 없습니다.'}, status=status.HTTP_403_FORBIDDEN)
-        owner_list = self.queryset.filter(is_owner=True)
-        serializer = owner_serializer(owner_list, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+# 유저 view를 상속하여 create만 재정의
+class owner_auth_view(user_auth):
 
     def create(self, request):
-        owner = self.queryset.create_user(
+        user = self.queryset.create_user(
+            # **request.data
             username=request.data['username'],
-            password=request.data['password'],
             email=request.data['email'],
-            sex=request.data['sex'],
             birth=request.data['birth'],
+            sex=request.data['sex'],
+            password=request.data['password'],
             is_owner=True,
             is_general=False
         )
-        serializer = owner_serializer(owner)
+        serializer = user_serializer(user)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    @action(detail=False, methods=['post'])
-    def login(self, request):
-        auth_owner = authenticate(self.request, username=request.data['username'], password=request.data['password'])
-        if auth_owner is None:
-            return Response({'정보가 일치하지 않습니다.'}, status=status.HTTP_401_UNAUTHORIZED)
-        login(request, auth_owner)
-        return Response(status=status.HTTP_200_OK)
-
-    @action(detail=False, methods=['get'])
-    def logout(self, request):
-        logout(request)
-        return Response(status=status.HTTP_200_OK)
-
-    def update(self):
-        return Response(status=status.HTTP_501_NOT_IMPLEMENTED)
-
-    def retrieve(self, request, *args, **kwargs):
-        if request.user == self.get_object():
-            data = super().retrieve(request, *args, **kwargs).data
-            return Response(data)
-        return Response({'권한이 없습니다.'}, status=status.HTTP_403_FORBIDDEN)
-
-    def partial_update(self, reuqest):
-        pass
-
-    def withdrawal(self):
-        pass
 
 # 리스틑, 등록, 업데이트, 삭제 모두 필요하므로 일단 viewset
 # 단, 업데이트와 삭제의 경우, 클라이언트의 요청이 아닌 서부 내부의 스태프만 처리할 수 있어야 하므로
